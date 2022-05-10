04478fafc2ecd6155f271b1a838b8758b46d6e85
Fireleque <vicesun@gmail.com>
2022-05-10 17:36:01 +0300

    lesson 4.2 step 8 - added new test methods

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
 from .pages.main_page import MainPage
-
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    page.should_be_login_link()      # проверяем, что есть ссылка, которая ведет на логин
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    page.should_be_login_link()  # проверяем, что есть ссылка, которая ведет на логин
+
+    login = LoginPage(browser, link)
+    login.should_be_login_form()
+    login.should_be_register_form()
+    login.should_be_login_url()
